ebdac0a2bc99bf96337ac1023ab5fbb963efd3c2
shiyao <shiyao@spacesystech.com>
2017-12-13 23:58:12 +0800

    gsy
    gsy

--- src/com/gsy/jdbc/JDBCConnect.java
+++ src/com/gsy/jdbc/JDBCConnect.java
@@ -7,20 +7,63 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class JDBCConnect {
 	public static void main(String[] args) {
-		System.out.println("测试idea   git提交");
-		String[] arr = new String[1];
-		List<a> list = new ArrayList<a>();
-		for (int i = 0; i < 4; i++) {
-			list.add(new a(i + "", i + ""));
+		A a1 = new A("1", 1, "A");
+		A a2 = new A("2", 2, "A");
+		A a3 = new A("3", 3, "A");
+		A a4 = new A("1", 1, "B");
+		A a5 = new A("2", 2, "B");
+		A a9 = new A("3", 3, "B");
+		A a6 = new A("1", 1, "C");
+		A a7 = new A("2", 2, "C");
+		A a8 = new A("3", 3, "C");
+		List<A> list = new ArrayList<A>();
+		list.add(a1);
+		list.add(a2);
+		list.add(a3);
+		list.add(a4);
+		list.add(a5);
+		list.add(a6);
+		list.add(a7);
+		list.add(a8);
+		list.add(a9);
+		String type = list.get(1).getType();
+		Map<String, Object> map = new HashMap();
+		for (int i = 0; i < list.size(); i++) {
+			if (type.equals(list.get(i).getType())) {
+				Set set = new HashSet();
+				set.add("id:"+list.get(i).getId());
+				set.add("name:"+list.get(i).getName());
+				set.add("type:"+list.get(i).getType());
+				map.put(list.get(i).getId(), set);
+				for (int j = 0; j < list.size(); j++) {
+					if (!type.equals(list.get(j).getType())&& list.get(i).getId()==list.get(j).getId()) {
+						set.add("add:"+list.get(j).getType());
+					}
+				}
+			}
 		}
-		System.out.println("==============");
-		Long time1 = new Date().getTime();
-		addTypes(list);
-		System.out.println(new Date().getTime() - time1);
+		for (String key : map.keySet()) {
+			System.out.println("key= " + key + " and value= " + map.get(key));
+		}
+
+		// connectJDBC(1);
+		// String[] arr = new String[1];
+		// List<a> list = new ArrayList<a>();
+		// for (int i = 0; i < 4; i++) {
+		// list.add(new a(i + "", i + ""));
+		// }
+		// System.out.println("==============");
+		// Long time1 = new Date().getTime();
+		// addTypes(list);
+		// System.out.println(new Date().getTime() - time1);
 		// Long time1 = new Date().getTime();
 		// for(int i = 0;i<4;i++) {
 		// arr[i]=String.valueOf(i);
@@ -61,12 +104,12 @@ public class JDBCConnect {
 	public static int addTypes(List<a> babyList) {
 		String sql = "insert into table_stu (id,name) values (?,?) ";
 		Connection conn = getCon(null);
-	    try {
+		try {
 			conn.setAutoCommit(false);
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}          
+		}
 		int result = 0;
 		PreparedStatement stmt = null;
 		try {
@@ -141,7 +184,7 @@ public class JDBCConnect {
 		String url = "jdbc:mysql://127.0.0.1:3306/own_test";
 		String username = "root";
 		String password = "123456";
-		String sql = "insert into table_stu(id,name) values(3,\"wangqian\")";
+		String sql = "SELECT *  from table_stu t";
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection(url, username, password);
@@ -150,20 +193,17 @@ public class JDBCConnect {
 			pStatement = (PreparedStatement) con.prepareStatement(sql);
 			// pStatement.setInt(1, pid);
 			pStatement.executeUpdate();
-			con.commit();
-			pStatement.close();
-			con.close();
 			// }else{
 			// statement = (Statement) con.createStatement();
 			// sql = sql.replace("?", " ");
 			// result = statement.executeQuery(sql+pid);
 			// }
 
-			// while (result.next()) {
-			// int id = result.getInt(1);
-			// String name = result.getString(2);
-			// System.out.println(id + "||" + name);
-			// }
+			while (result.next()) {
+				int id = result.getInt(1);
+				String name = result.getString(2);
+				System.out.println(id + "||" + name);
+			}
 		} catch (Exception e) {
 			try {
 				con.rollback();
@@ -179,30 +219,44 @@ public class JDBCConnect {
 
 }
 
-class a {
+class A {
 	String id;
-	String name;
+	int name;
+	String type;
 
 	public String getId() {
 		return id;
+	}
+
+	public A() {
+		super();
 	}
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
-	public String getName() {
+	public int getName() {
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setName(int name) {
 		this.name = name;
 	}
 
-	public a(String id, String name) {
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public A(String id, int name, String type) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.type = type;
 	}
 
 }
