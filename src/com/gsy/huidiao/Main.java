e93a837fff1f7db83e4113096ac7a78948a6bd1c
gaoshiyao <shiyao@spacesystech.com>
2017-12-13 15:57:28 +0800

    eclipse提交

--- src/com/gsy/huidiao/Main.java
+++ src/com/gsy/huidiao/Main.java
@@ -2,6 +2,7 @@ package com.gsy.huidiao;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("测试eclipse提交");
 		MyService myService = new MyService();
 		BWriteCall bWriteCall = new BWriteCall();
 		myService.setCallFunc(bWriteCall);
