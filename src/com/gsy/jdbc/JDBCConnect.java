c499affb9a2282b89b4aa6d86f17f3532b2ecb2c
gaoshiyao <shiyao@spacesystech.com>
2017-12-14 10:07:36 +0800

    jdbc整理

--- src/com/gsy/jdbc/JDBCConnect.java
+++ src/com/gsy/jdbc/JDBCConnect.java
@@ -15,138 +15,86 @@ import java.util.Set;
 
 public class JDBCConnect {
 	public static void main(String[] args) {
-		A a1 = new A("1", 1, "A");
-		A a2 = new A("2", 2, "A");
-		A a3 = new A("3", 3, "A");
-		A a4 = new A("1", 1, "B");
-		A a5 = new A("2", 2, "B");
-		A a9 = new A("3", 3, "B");
-		A a6 = new A("1", 1, "C");
-		A a7 = new A("2", 2, "C");
-		A a8 = new A("3", 3, "C");
+		connectJDBC(1);
+		String[] arr = new String[1];
 		List<A> list = new ArrayList<A>();
-		list.add(a1);
-		list.add(a2);
-		list.add(a3);
-		list.add(a4);
-		list.add(a5);
-		list.add(a6);
-		list.add(a7);
-		list.add(a8);
-		list.add(a9);
-		String type = list.get(1).getType();
-		Map<String, Object> map = new HashMap();
-		for (int i = 0; i < list.size(); i++) {
-			if (type.equals(list.get(i).getType())) {
-				Set set = new HashSet();
-				set.add("id:"+list.get(i).getId());
-				set.add("name:"+list.get(i).getName());
-				set.add("type:"+list.get(i).getType());
-				map.put(list.get(i).getId(), set);
-				for (int j = 0; j < list.size(); j++) {
-					if (!type.equals(list.get(j).getType())&& list.get(i).getId()==list.get(j).getId()) {
-						set.add("add:"+list.get(j).getType());
-					}
-				}
-			}
+		for (int i = 0; i < 4; i++) {
+			list.add(new A(i + "", i,""));
 		}
-		for (String key : map.keySet()) {
-			System.out.println("key= " + key + " and value= " + map.get(key));
-		}
-
-		// connectJDBC(1);
-		// String[] arr = new String[1];
-		// List<a> list = new ArrayList<a>();
-		// for (int i = 0; i < 4; i++) {
-		// list.add(new a(i + "", i + ""));
-		// }
-		// System.out.println("==============");
-		// Long time1 = new Date().getTime();
-		// addTypes(list);
-		// System.out.println(new Date().getTime() - time1);
-		// Long time1 = new Date().getTime();
-		// for(int i = 0;i<4;i++) {
-		// arr[i]=String.valueOf(i);
-		// }
-		// arr[0]="1";
-		// for(int i = 0;i<50;i++) {
-		// connectJDBC(1);
-		// connectJDBC(1);
-		// connectJDBC(1);
-		// connectJDBC(1);
-		// }
-		// System.out.println(new Date().getTime()-time1);
-		//
-		// Connection conn = getCon(null);
-		// if (conn != null) {
-		// System.out.println("连接成功");
-		// //多线程插入
-		// Long time = new Date().getTime();
-		// for(int i = 0;i<50;i++) {
-		// Thread t1 = new Thread(new InsertThread(conn,arr));
-		// t1.run();
-		// }
-		//
-		//
-		// Thread t1 = new Thread(new InsertThread(conn,arr));
-		// Thread t2 = new Thread(new InsertThread(conn,arr));
-		// Thread t3 = new Thread(new InsertThread(conn,arr));
-		// Thread t4 = new Thread(new InsertThread(conn,arr));
-		// t1.run();
-		// t2.run();
-		// t3.run();
-		// t1.run();
-		// System.out.println(new Date().getTime()-time);
-		// }
-		// closeJDBC(conn);
+		System.out.println("==============");
+		Long time1 = new Date().getTime();
+		addTypes(list);
+		System.out.println(new Date().getTime() - time1);
+		System.out.println("==============");
+//		Connection conn = getCon(null);
+//		if (conn != null) {
+//			System.out.println("连接成功");
+//			// 多线程插入
+//			Long time = new Date().getTime();
+//			for (int i = 0; i < 50; i++) {
+//				Thread t1 = new Thread(new InsertThread(conn, arr));
+//				t1.run();
+//			}
+//			Thread t1 = new Thread(new InsertThread(conn, arr));
+//			Thread t2 = new Thread(new InsertThread(conn, arr));
+//			Thread t3 = new Thread(new InsertThread(conn, arr));
+//			Thread t4 = new Thread(new InsertThread(conn, arr));
+//			t1.run();
+//			t2.run();
+//			t3.run();
+//			t1.run();
+//			System.out.println(new Date().getTime() - time);
+//		}
+//		closeJDBC(conn);
 	}
 
-//	public static int addTypes(List<a> babyList) {
-//		String sql = "insert into table_stu (id,name) values (?,?) ";
-//		Connection conn = getCon(null);
-//		try {
-//			conn.setAutoCommit(false);
-//		} catch (SQLException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//		int result = 0;
-//		PreparedStatement stmt = null;
-//		try {
-//			stmt = conn.prepareStatement(sql);
-//			for (int i = 1; i < babyList.size(); i++) {
-//				if (i == 2) {
-//					stmt.setString(1, "1");
-//					stmt.setString(2, "2");
-//					stmt.addBatch();
-//					continue;
-//				}
-//				stmt.setString(1, babyList.get(i).getId());
-//				stmt.setString(2, babyList.get(i).getName());
-//				stmt.addBatch();
-//				stmt.executeBatch();
-//			}
-//			conn.commit();
-//		} catch (Exception e) {
-//			try {
-//				conn.rollback();
-//			} catch (SQLException e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
-//			e.printStackTrace();
-//		} finally {
-//			if (stmt != null) {
-//				try {
-//					stmt.close();
-//				} catch (SQLException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//			closeJDBC(conn);
-//		}
-//		return result;
-//	}
+	// 批量插入
+	public static int addTypes(List<A> babyList) {
+		String sql = "insert into table_stu (id,name) values (?,?) ";
+		Connection conn = getCon(null);
+		try {
+			conn.setAutoCommit(false);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		int result = 0;
+		PreparedStatement stmt = null;
+		try {
+			stmt = conn.prepareStatement(sql);
+			for (int i = 1; i < babyList.size(); i++) {
+				if (i == 2) {
+					stmt.setString(1, "1");
+					stmt.setString(2, "2");
+					stmt.addBatch();
+					continue;
+				}
+				stmt.setString(1, babyList.get(i).getId());
+				stmt.setInt(2, babyList.get(i).getName());
+				stmt.addBatch();
+				stmt.executeBatch();
+			}
+			conn.commit();
+		} catch (Exception e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		} finally {
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			closeJDBC(conn);
+		}
+		return result;
+	}
 
 	// 获取连接
 	public static Connection getCon(Connection con) {
@@ -176,12 +124,13 @@ public class JDBCConnect {
 		}
 	}
 
+	// 两种操作
 	public static void connectJDBC(int pid) {
 		Connection con = null;
 		// Statement statement = null;
 		PreparedStatement pStatement = null;
 		ResultSet result = null;
-		String url = "jdbc:mysql://127.0.0.1:3306/own_test";
+		String url = "jdbc:mysql://127.0.0.1:3306/gsy_local";
 		String username = "root";
 		String password = "123456";
 		String sql = "SELECT *  from table_stu t";
@@ -192,13 +141,12 @@ public class JDBCConnect {
 			// if(type==1){
 			pStatement = (PreparedStatement) con.prepareStatement(sql);
 			// pStatement.setInt(1, pid);
-			pStatement.executeUpdate();
+			result = pStatement.executeQuery();
 			// }else{
 			// statement = (Statement) con.createStatement();
 			// sql = sql.replace("?", " ");
 			// result = statement.executeQuery(sql+pid);
 			// }
-
 			while (result.next()) {
 				int id = result.getInt(1);
 				String name = result.getString(2);
